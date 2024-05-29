dd1a40b18fd79d5c95e65766cf37a0a9b8e22d91
Derick <116215531+erick1293@users.noreply.github.com>
2024-05-29 18:35:56 -0400

    xd
    xd

---
+++ src/Base_Datos/obtener_marcas.php
@@ -0,0 +1,16 @@
+<?php
+// Conexión a la base de datos y consulta para obtener las marcas
+include 'conection.php';
+
+$query = "SELECT * FROM marca";
+$resultado = mysqli_query($conn, $query);
+$marcas = array();
+
+while ($row = mysqli_fetch_assoc($resultado)) {
+    $marcas[] = $row;
+}
+
+echo json_encode($marcas);
+
+mysqli_close($conn);
+?>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../stylesheets/navbar.css'; // Importa el archivo CSS de estilos
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/AgendarCita">Agendar Cita</a></li>
+        <li><a href="/AgregarAuto">Agregar Auto</a></li>
+        <li><a href="/AgregarObjeto">Agregar Objeto</a></li>
+        <li><a href="/Estados">Estado</a></li>
+        {/* Dropdown Vehículo */}
+        <li className="dropdown">
+          <a href="#" className="dropbtn">Vehículo</a>
+          <div className="dropdown-content">
+            <a href="/AgregarVehiculo">Agregar</a>
+            <a href="/ModificarVehiculos">Modificar</a>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
+
