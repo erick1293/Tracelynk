2ee9e78d7d608f8ddfd9a130afbb3d8f0df002cb
erick1293 <erick.meneses1293@gmail.com>
2024-06-06 11:45:12 -0400

    "Removed Navbar, AgregarAuto, Navegador, Maps, and MenuDerecha imports and routes, added Inicio and Mantencion imports and routes, and updated route paths and elements."

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes , Switch} from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AgendarCita from './components/AgendarCita';
-import AgregarAuto from './components/AgregarAuto';
 import AgregarObjeto from './components/AgregarObjeto';
 import Estados from './components/Estados';
 import AgregarVehiculo from "./Axios/Agregar_axios"
 import ModificarVehiculos from "./Axios/Modificar_vehiculos"
-import {Navegador,Maps,MenuDerecha} from "./components/home"
-import Map from "./components/Mapa/index"
+import Inicio from "./components/Inicio"
+import Mantencion from "./Axios/AgregarMantencion" 
+
 function App() {
   return (
     <>
       <BrowserRouter>
       <Routes>
-     
-        <Route path='/' element={<Navbar/>}/> 
-        <Route path='/Home' element={<MenuDerecha/>}/> 
-        <Route path='/Map' element={<Map/>}/> 
+        <Route path='/' element={<Inicio/>}/> 
         <Route path='/AgendarCita' element={<AgendarCita/>}/>
-        <Route path='/AgregarAuto' element={<AgregarAuto/>}/>
+        <Route path='/AgendarMantencion' element={<Mantencion/>}/>
         <Route path='/AgregarObjeto' element={<AgregarObjeto/>}/>
         <Route path='/Estados' element={<Estados/>}/> 
         <Route path='/AgregarVehiculo' element={<AgregarVehiculo/>}/> 
