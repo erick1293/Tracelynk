d2523366c42957c443dfad3f62ef52faeaf3d844
erick1293 <erick.meneses1293@gmail.com>
2024-06-06 11:51:10 -0400

    asd

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import {MenuDerecha} from "./home.jsx"
+import Navbar from './Navbar'; 
+import {Maps , MenuDerecha} from "./Home"
+import '../stylesheets/Home.css';
 
-function inicio () {
-
-
-
+function Home() {
+  return (
+<> 
+      <Navbar />
+      <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <Maps />
+        </div>
+        
+        <div className="col-md-6">
+          <MenuDerecha />
+        </div>
+      </div>
+    </div>
+</>
+  );
 }
+
+export default Home;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,47 +1,42 @@
 import React from 'react';
 import Navbar from './Navbar'; // Importa el componente Navbar
+import "../stylesheets/Home.css";
 
-
-export function MenuDerecha() {
+export function Home() {
   return (
-    <div>
-      <Navbar /> 
-      <div className="menuDerecho">
-        <div className="menuDerecho1">
-          <h2 className="parametro">Parametros</h2>
-          <ul id="misParametros">
-            <li>Temperatura de motor: (80)</li>
-            <li>Combustible usado: (40 LTS)</li>
-            <li>Tiempo detenido: (15 Min)</li>
-            <li>Ubicación: (AVDA Los loros)</li>
-            <li>Siguiente mantención: (8.000 KM)</li>
-          </ul>
+    <div className="container">
+      <Navbar />
+      <div className="row">
+        <div className="col-md-6">
+          <Maps />
+        </div>
+        <div className="col-md-6">
+          <MenuDerecha />
         </div>
       </div>
     </div>
   );
 }
 
+export function MenuDerecha() {
+  return (
+    <div className="menuDerecho">
+        <h2 className="parametro">Parametros</h2>
+        <ul id="misParametros"> 
+          <li>Temperatura de motor: (80)</li><br/>
+          <li>Combustible usado: (40 LTS)</li><br/>
+          <li>Tiempo detenido: (15 Min)</li><br/>
+          <li>Ubicación: (AVDA Los loros)</li><br/>
+          <li>Siguiente mantención: (8.000 KM)</li><br/>
+        </ul>
+      </div>
+  );
+}
+
 export function Maps() {
   return (
     <div>
-      <Navbar /> {/* Incluye el Navbar aquí */}
       <p>aqui va el mapa en la zona izquierda</p>
     </div>
   );
 }
-
-export function Navegador() {
-  return (
-    <div>
-      <Navbar /> {/* Incluye el Navbar aquí */}
-      <div className="Navegador">
-        <a href="#home">Inicio</a>
-        <a href="#about">Acerca de</a>
-        <a href="#contact">Contacto</a>
-      </div>
-    </div>
-  );
-}
-
-export default {MenuDerecha,Maps, Navegador};
