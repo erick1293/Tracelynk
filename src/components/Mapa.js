f234663bf453ca5613f40bc53096b97c3e58f860
Derick <116215531+erick1293@users.noreply.github.com>
2024-11-13 16:58:15 -0300

    sdd

--- src/components/Mapa.js
+++ src/components/Mapa.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { MapContainer, TileLayer, Marker, Polygon } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
-// Fix marker icons issue with Leaflet in React
 delete L.Icon.Default.prototype._getIconUrl;
-
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
   iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
@@ -14,168 +12,54 @@ L.Icon.Default.mergeOptions({
 });
 
 const Mapa = () => {
-  const [allPoints, setAllPoints] = useState([]);
-  const [polygonCoordinates, setPolygonCoordinates] = useState([]);
-  const [poligonos, setPoligonos] = useState([]);
-  const [selectedPoligono, setSelectedPoligono] = useState(null);
-  const [ubicaciones, setUbicaciones] = useState([]);
-  const [position, setPosition] = useState([-27.360535800413754, -70.3350422675603]); // Default position
-  const [selectedUbicacion, setSelectedUbicacion] = useState("");
+  const [position, setPosition] = useState([-27.3605358, -70.3350422]);
 
-  // Obtener los polígonos disponibles
+  // Obtener ubicación GPS y enviar al servidor cada 10 segundos
   useEffect(() => {
-    axios.get('http://ec2-54-221-134-204.compute-1.amazonaws.com/poligonos/MostrarPoligonos.php')
-      .then(response => {
-        setPoligonos(response.data);
-      })
-      .catch(error => {
-        console.error('Hubo un error al obtener los polígonos:', error);
-      });
+    const updateLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (pos) => {
+            const { latitude, longitude } = pos.coords;
+            setPosition([latitude, longitude]);
+            sendLocationToServer(latitude, longitude);
+          },
+          (error) => console.error("Error obteniendo ubicación:", error)
+        );
+      } else {
+        console.log("Geolocalización no disponible.");
+      }
+    };
 
-    // Obtener todos los puntos una vez
-    axios.get('http://ec2-54-221-134-204.compute-1.amazonaws.com/poligonos/MostrarPunto.php')
-      .then(response => {
-        setAllPoints(response.data);
-      })
-      .catch(error => {
-        console.error('Error al obtener los puntos de los polígonos:', error);
-      });
+    // Actualizar ubicación cada 10 segundos
+    updateLocation(); // Actualización inicial
+    const intervalId = setInterval(updateLocation, 10000); // 10 segundos
 
-    // Obtener la ubicación del vehículo
-    axios.get('http://ec2-54-221-134-204.compute-1.amazonaws.com/poligonos/MostrarUbicacion.php')
-      .then(response => {
-        setUbicaciones(response.data);
-        if (response.data.length > 0) {
-          const ubicacion = response.data[0]; // Usar la primera ubicación como la posición del vehículo
-          setPosition([parseFloat(ubicacion.latitud), parseFloat(ubicacion.longitud)]);
-        }
-      })
-      .catch(error => {
-        console.error('Error al obtener la ubicación del vehículo:', error);
-      });
+    return () => clearInterval(intervalId); // Limpia el intervalo al desmontar
   }, []);
 
-  // Filtrar los puntos del polígono seleccionado
-  useEffect(() => {
-    if (selectedPoligono) {
-      const filteredPoints = allPoints
-        .filter(punto => punto.Poligono_idPoligono === selectedPoligono)
-        .map(punto => [parseFloat(punto.Latitud), parseFloat(punto.Longitud)]);
-      setPolygonCoordinates(filteredPoints);
-    } else {
-      setPolygonCoordinates([]);
+  const sendLocationToServer = async (latitude, longitude) => {
+    try {
+      const response = await axios.post(
+        'http://ec2-54-221-134-204.compute-1.amazonaws.com/poligonos/guardar_ubicacion.php',
+        { latitude, longitude },
+        { headers: { "Content-Type": "application/json" } }
+      );
+      console.log("Ubicación en tiempo real enviada:", response.data);
+    } catch (error) {
+      console.error("Error al enviar ubicación en tiempo real:", error);
     }
-  }, [selectedPoligono, allPoints]);
-
-  const enviarAlerta = () => {
-      axios.post('http://ec2-54-221-134-204.compute-1.amazonaws.com/Alertas/Envia_Alertas.php', {
-          alert: true
-      })
-      .then(response => {
-          console.log(response.data.message);
-      })
-      .catch(error => {
-          console.error("Hubo un error al enviar la alerta:", error);
-      });
-  };
-  
-  // Llama a esta función cuando necesites enviar la alerta
-  enviarAlerta();
-  
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-        setSelectedUbicacion(prevSelectedUbicacion => {
-            const currentIndex = ubicaciones.findIndex(ubicacion => ubicacion.idUbicacion === prevSelectedUbicacion);
-            const nextIndex = (currentIndex + 1) % ubicaciones.length;
-            const nextUbicacion = ubicaciones[nextIndex];
-            
-            if (nextUbicacion) {
-                const newLat = parseFloat(nextUbicacion.latitud);
-                const newLng = parseFloat(nextUbicacion.longitud);
-
-                // Verifica si la latitud y longitud son válidas
-                if (!isNaN(newLat) && !isNaN(newLng)) {
-                    setPosition([newLat, newLng]);
-
-                    // Verificar si la ubicación del vehículo está dentro del polígono
-                    if (polygonCoordinates.length > 0) {
-                        const vehicleLocation = L.latLng(newLat, newLng);
-                        const polygon = L.polygon(polygonCoordinates);
-                        if (!polygon.getBounds().contains(vehicleLocation)) {
-                            console.log("¡Alerta! El vehículo ha salido del polígono");
-
-                            // Enviar alerta al servidor PHP
-                            axios.post('http://ec2-54-221-134-204.compute-1.amazonaws.com/Alertas/Envia_Alertas.php', {
-                                alert: true
-                            })
-                            .then(response => {
-                                console.log(response.data.message);
-                            })
-                            .catch(error => {
-                                console.error('Error al enviar la alerta:', error);
-                            });
-                        }
-                    }
-                }
-            }
-            return nextUbicacion.idUbicacion;
-        });
-    }, 20000000); // 2000 para que avance normal
-
-    return () => clearInterval(intervalId);
-}, [ubicaciones, polygonCoordinates]);
-
-  
-
-  const handlePoligonoChange = (e) => {
-    setSelectedPoligono(e.target.value);
-  };
-
-  const handleUbicacionChange = (e) => {
-    const ubicacionSeleccionada = ubicaciones.find(ubicacion => ubicacion.idUbicacion === e.target.value);
-    if (ubicacionSeleccionada) {
-      setPosition([parseFloat(ubicacionSeleccionada.latitud), parseFloat(ubicacionSeleccionada.longitud)]);
-    }
-    setSelectedUbicacion(e.target.value);
   };
 
   return (
-    <div>
-      <div>
-        <select onChange={handlePoligonoChange}>
-          <option value="">Seleccione un polígono</option>
-          {poligonos.map((poligono) => (
-            <option key={poligono.idPoligono} value={poligono.idPoligono}>
-              {poligono.nombre}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <select value={selectedUbicacion} onChange={handleUbicacionChange}>
-          <option value="">Seleccione una ubicación</option>
-          {ubicaciones.map((ubicacion) => (
-            <option key={ubicacion.idUbicacion} value={ubicacion.idUbicacion}>
-              Lat: {ubicacion.latitud}, Lon: {ubicacion.longitud}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div style={{ height: "900px", width: "100%" }}>
-        <MapContainer center={position} zoom={14} style={{ height: "100%", width: "100%" }}>
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          <Marker position={position}></Marker>
-          {polygonCoordinates.length > 0 && (
-            <Polygon positions={polygonCoordinates} color="purple"></Polygon>
-          )}
-        </MapContainer>
-      </div>
-    </div>
+    <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position} />
+    </MapContainer>
   );
-}
+};
 
 export default Mapa;
- 
